94faae1f44c728a921e9e2d6a39c3400a925f8a5
Suseenthiran Arulraj Rumeasiyan <rumeasiyan@dxdy.tech>
2024-06-22 23:42:49 +0530

    commiting changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Movie List</title>
+    <title>Movies List</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
